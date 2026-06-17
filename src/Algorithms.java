f72e420d0ddb4967313fe9bb17341506f3c1b742
NaEl-123 <naggrey001@st.ug.edu.gh>
2026-06-17 11:08:26 +0000

    Update Algorithms.java

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,4 +1,5 @@
-public class Algorithms {static int linearSearch(int[] arr, int target)
+public class Algorithms {
+    static int linearSearch(int[] arr, int target)
     {
         for(int i=0;i<arr.length;i++)
         {
